a7bfeebeedc2987899dcf808df18f6db7ad09882
Kamil <kam.palinski@gmail.com>
2023-09-27 18:42:25 +0200

    feat: add fetch data functionality, loading spinnerand pagination component

--- src/components/views/mainView/MainView.tsx
+++ src/components/views/mainView/MainView.tsx
@@ -1,13 +1,42 @@
+import { useState, useEffect, useContext } from 'react';
 import { Box } from '@mui/material';
 
-import { CardList } from 'components';
+import { CardList, Pagination, Spinner } from 'components';
+import { useApi } from 'hooks';
+import { PagesContext } from 'context';
+import { ProductTypes } from 'types';
 
 import { Container } from './MainView.styles';
 
 export const MainView = () => {
+  const [cards, SetCards] = useState<Array<ProductTypes>>([]);
+  const { fetchData, isLoading } = useApi();
+  const { actualPage } = useContext(PagesContext);
+  const queryParamsPage = {
+    page: actualPage.toString(),
+    per_page: '20',
+  };
+  console.log(isLoading);
+  useEffect(() => {
+    fetchData('getBeers', dataHandler, queryParamsPage);
+  }, [actualPage]);
+  const dataHandler = (items: ProductTypes[]) => {
+    items && SetCards(items);
+  };
+
   return (
     <Box sx={Container}>
-      <CardList />
+      <>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <>
+            <CardList cards={cards} />
+
+            <Pagination />
+          </>
+        )}
+      </>
     </Box>
   );
 };
