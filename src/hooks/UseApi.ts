e40d754ec755aeec4d2be85b2ffe9176119e0587
Kamil <kam.palinski@gmail.com>
2023-09-27 18:43:10 +0200

    feat: change fetch metchod names

--- src/hooks/UseApi.ts
+++ src/hooks/UseApi.ts
@@ -5,15 +5,20 @@ import { ProductTypes } from 'types';
 export const useApi = () => {
   const [isLoading, setIsLoading] = useState(false);
   const fetchData = useCallback(
-    async (method: string, catchData: (event: ProductTypes[]) => void, queryParams?: string | undefined) => {
+    async (
+      method: string,
+      catchData: (event: ProductTypes[]) => void,
+      queryParams?: { [key: string]: string } | string,
+    ) => {
+      const query = new URLSearchParams(queryParams).toString();
       let response;
       try {
         setIsLoading(true);
-        if (method === 'GET') {
-          response = await fetch(`https://api.punkapi.com/v2/beers`);
-        }
         if (method === 'getDetail') {
           response = await fetch(`https://api.punkapi.com/v2/beers/${queryParams}`);
+        }
+        if (method === 'getBeers') {
+          response = await fetch(`https://api.punkapi.com/v2/beers?${query}`);
         }
         const responseData = await response?.json();
         const sentData = catchData?.(responseData);
