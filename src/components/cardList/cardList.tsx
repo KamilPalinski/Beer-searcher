b3de60f2fa59905dd3412cb42f941b3673ccf3c7
Kamil <kam.palinski@gmail.com>
2023-09-27 18:40:02 +0200

    feat: delete fetch data functionality

--- src/components/cardList/cardList.tsx
+++ src/components/cardList/cardList.tsx
@@ -1,21 +1,14 @@
-import { useState, useEffect } from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 
 import { Spinner } from 'components';
 import { Card } from 'components';
 import { useApi } from 'hooks';
-import { ProductTypes } from 'types';
+import { CardListTypes } from 'types';
 
 import { Container, GridContainer, GridElement } from './cardList.styles';
-export const CardList = () => {
-  const [cards, SetCards] = useState<Array<ProductTypes>>([]);
-  useEffect(() => {
-    fetchData('GET', dataHandler);
-  }, []);
-  const dataHandler = (items: ProductTypes[]) => {
-    items && SetCards(items);
-  };
-  const { fetchData, isLoading } = useApi();
+
+export const CardList = ({ cards }: CardListTypes) => {
+  const { isLoading } = useApi();
 
   return (
     <Box sx={Container}>
@@ -25,19 +18,16 @@ export const CardList = () => {
         <>
           <Box sx={GridContainer}>
             {cards &&
-              cards.map(
-                (items, index) =>
-                  index < 5 && (
-                    <Card
-                      sx={GridElement}
-                      id={items.id}
-                      key={items.name}
-                      name={items.name}
-                      image_url={items.image_url}
-                      tagline={items.tagline}
-                    />
-                  ),
-              )}
+              cards.map((items) => (
+                <Card
+                  sx={GridElement}
+                  id={items.id}
+                  key={items.name}
+                  name={items.name}
+                  image_url={items.image_url}
+                  tagline={items.tagline}
+                />
+              ))}
           </Box>
         </>
       )}
